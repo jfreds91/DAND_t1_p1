0de2c8e2f17735af33013b3c586218a4c99a1d61
jfreds91 <jfreds@bu.edu>
2018-07-15 17:24:12 -0700

    Add files via upload

--- t1-p1.py
+++ t1-p1.py
@@ -4,6 +4,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # SQL: search through available cities in city_list
 # SELECT *
@@ -27,18 +28,47 @@ pth = 'C:/Users/Jesse/Documents/PyData/DAND_t1_p1/'
 
 df_boston = pd.read_csv(pth + 'boston_weather.csv')
 df_global = pd.read_csv(pth + 'global_weather.csv')
-# df_global.rename(index=str, columns={'year': 'year_global', 'avg_temp': 'avg_temp_global'}, inplace=True)
 
 # join dataframes by year
-df = df_boston.set_index('year').join(df_global.set_index('year'), rsuffix='_global',)
+df = df_boston.set_index('year').join(df_global.set_index('year'), rsuffix='_global', )
 
 # get rolling mean columns for boston and global weather
 df['boston_rolling_temp'] = df['avg_temp'].rolling(10, min_periods=10).mean()
 df['global_rolling_temp'] = df['avg_temp_global'].rolling(10, min_periods=10).mean()
 
-# Plot
-ax = df.plot(y=['avg_temp', 'avg_temp_global'])
-ax.legend(['Boston', 'Global'])
-plt.show()
+# clean missing data
+df_clean = df.dropna(subset=['boston_rolling_temp', 'global_rolling_temp'], how='any')
 
-print('done')
+# form linear regression trendlines
+trend_bos = np.polyfit(df_clean.index, df_clean['boston_rolling_temp'], 3)
+bos_trend_p = np.poly1d(trend_bos)
+df_clean['trend_bos'] = bos_trend_p(df_clean.index)
+
+trend_global = np.polyfit(df_clean.index, df_clean['global_rolling_temp'], 3)
+global_trend_p = np.poly1d(trend_global)
+df_clean['trend_global'] = global_trend_p(df_clean.index)
+
+
+def r_squared(y, y_fit):
+    # returns the coefficient of determination for y and y_fit
+    ybar = np.sum(y) / len(y)
+    ssreg = np.sum((y_fit - ybar) ** 2)
+    sstot = np.sum((y - ybar) ** 2)
+    determination = ssreg / sstot
+
+    return determination
+
+
+# get R^2 values for the fit lines
+boston_r2 = r_squared(df_clean['boston_rolling_temp'], df_clean['trend_bos'])
+global_r2 = r_squared(df_clean['global_rolling_temp'], df_clean['trend_global'])
+
+# Plot
+df_clean.plot(y=['boston_rolling_temp', 'global_rolling_temp', 'trend_bos', 'trend_global'])
+plt.legend(['Boston', 'Global', 'Boston Trend' + ' (R2 {:.3f})'.format(boston_r2),
+            'Global Trend' + ' (R2 {:.3f})'.format(global_r2)])
+plt.title('Local and Global Temperatures')
+plt.ylabel('Temperature (C)')
+plt.xlabel('Year')
+plt.ylim((5, 10))
+plt.show()
